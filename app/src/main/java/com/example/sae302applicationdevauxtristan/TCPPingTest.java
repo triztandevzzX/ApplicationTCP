bb2c88b0f55bdb40be7c0ca8ad0bfaa68ee70ad0
Tristan <tristandevaux6@icloud.com>
2024-01-08 15:46:39 +0100

    Commit 5 - 08 Janvier 2024
    - Ajout "TCPPingTest" dans le dossier "Java"
    - Création code XML de la page "activity_tcp_ping_test.xml" dans le dossier "layout"
    - Liaison des deux fichiers créés
    - Résolution de bug d'affichage sur le téléphone
    - Ajout logo IUT sur la page d'acceuil
    - Visuel de l'application quasiment terminé (80%)

--- app/src/main/java/com/example/sae302applicationdevauxtristan/TCPPingTest.java
+++ app/src/main/java/com/example/sae302applicationdevauxtristan/TCPPingTest.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TCPServerActivity extends AppCompatActivity {
+public class TCPPingTest extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tcp_server);
+        setContentView(R.layout.activity_tcp_ping_test);
     }
 }
